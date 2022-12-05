a92a730bfa38d6ff9087fc605938b0d53a269d5f
pepega-123 <goodgoose14@gmail.com>
2022-12-05 18:50:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hz2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hz2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View v) {
+        ImageView image = findViewById(R.id.imageView);
+        image.setBackgroundResource(R.drawable.result);
+        image.setVisibility(View.VISIBLE);
+        startService(new Intent(this, MyService.class).putExtra("time", 7));
+        startService(new Intent(this, MyService.class).putExtra("time", 2));
+        startService(new Intent(this, MyService.class).putExtra("time", 4));
+    }
+}
